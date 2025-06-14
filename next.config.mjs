f448f55c3d1dab00efdfaac2788a1a632f270743
alfian yuanata <alfianyuanata@gmail.com>
2025-06-14 14:47:15 +0700

    update the policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const ContentSecurityPolicy = `
+  default-src 'self';
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https://d2n7cdqdrlte95.cloudfront.net https://d2f3dnusg0rbp7.cloudfront.net https://api.sandbox.midtrans.com https://pay.google.com https://js-agent.newrelic.com https://bam.nr-data.net https://app.sandbox.midtrans.com https://www.google.com https://www.gstatic.com;
-  script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://d2n7cdqdrlte95.cloudfront.net https://d2f3dnusg0rbp7.cloudfront.net https://api.sandbox.midtrans.com https://pay.google.com https://js-agent.newrelic.com https://bam.nr-data.net https://app.sandbox.midtrans.com https://www.google.com https://www.gstatic.com;
   style-src 'self' 'unsafe-inline';
   img-src 'self' data: blob: https://res.cloudinary.com;
   connect-src 'self' https://api.sandbox.midtrans.com;
